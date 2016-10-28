abdfecc943020732ba6ec31840c76d96c6c98a5f
Miki Riglos <miki.riglos@live.com>
2016-10-28 12:11:29 -0400

    basic web app

---
+++ TableGames/src/TableGames/Controllers/HomeController.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace TableGames.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index() {
+            return View();
+        }
+
+        public IActionResult About() {
+            ViewData["Message"] = "Table Games web application.";
+
+            return View();
+        }
+
+        public IActionResult Error() {
+            return View();
+        }
+    }
+}
