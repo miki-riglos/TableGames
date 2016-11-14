28f4026673af680bfe64e3ba1a1f815b6e4b2e96
Miki Riglos <miki.riglos@live.com>
2016-11-14 18:37:17 -0500

    tic tac toe check for winner

--- TableGames.Web/Games/TicTacToe.cs
+++ TableGames.Web/Games/TicTacToe.cs
@@ -7,29 +7,43 @@ namespace TableGames.Web.Games
 {
     public class TicTacToe : Game
     {
-        private List<Box> _board;
+        public List<AssignedBox> Board { get; private set; }
+        public bool IsFinalized { get; private set; }
+        public List<AssignedBox> WinningBoxes { get; private set; }
+        public string WinnerName { get; private set; }
 
         public TicTacToe(Table table) : base(table) {
+            Board = createBoard();
+            IsFinalized = false;
+            WinningBoxes = new List<AssignedBox>();
+            WinnerName = null;
+
             SetNextPlayer();
-            _board = createBoard();
         }
 
-        private List<Box> createBoard() {
+        private List<AssignedBox> createBoard() {
             int[] indices = { 1, 2, 3 };
-            return indices.SelectMany(row => indices, (row, col) => new Box(row, col)).ToList();
+            return indices.SelectMany(row => indices, (row, col) => new AssignedBox(row, col)).ToList();
         }
 
-        public object AssignBox(string playerName, int row, int col) {
-            var box = _board.First(b => b.Row == row && b.Col == col);
-            if (box != null && box.PlayerName == null) {
-                box.PlayerName = playerName;
-                SetNextPlayer();
+        private object assignBox(int row, int col) {
+            var assignedBox = Board.First(ab => ab.Box.Row == row && ab.Box.Col == col);
+            if (assignedBox != null && assignedBox.PlayerName == null) {
+                assignedBox.PlayerName = ActivePlayer.Name;
+
+                updateState();
+
+                if (!IsFinalized) {
+                    SetNextPlayer();
+                }
 
                 return new {
                     row = row,
                     col = col,
-                    playerName = playerName,
-                    activePlayerName = ActivePlayer.Name
+                    activePlayerName = ActivePlayer.Name,
+                    isFinalized = IsFinalized,
+                    winningBoxes = WinningBoxes.Select(ab => ab.ToClient()),
+                    winnerName = WinnerName
                 };
             }
             else {
@@ -37,10 +51,24 @@ namespace TableGames.Web.Games
             }
         }
 
+        private void updateState() {
+            var playerBoxes = Board.Where(ab => ab.PlayerName == ActivePlayer.Name);
+            var winningBoxCombination = _winningBoxCombinations.FirstOrDefault(ids => ids.Intersect(playerBoxes.Select(ab => ab.Box.Id)).Count() == 3);
+
+            if (winningBoxCombination == null) {
+                IsFinalized = Board.Count(ab => ab.PlayerName != null) >= 9;
+            }
+            else {
+                IsFinalized = true;
+                WinningBoxes = Board.Where(ab => winningBoxCombination.Contains(ab.Box.Id)).ToList();
+                WinnerName = ActivePlayer.Name;
+            }
+        }
+
         public override object Change(string playerName, string eventName, dynamic gameChangeParameters) {
-            if (eventName == "AssignBox") {
-                if (playerName == ActivePlayer.Name ) {
-                    return AssignBox(playerName, (int)gameChangeParameters.row, (int)gameChangeParameters.col);
+            if (eventName == "assignBox") {
+                if (playerName == ActivePlayer.Name) {
+                    return assignBox((int)gameChangeParameters.row, (int)gameChangeParameters.col);
                 }
                 throw new Exception("TicTacToe Change error - It is not your turn.");
             }
@@ -49,30 +77,48 @@ namespace TableGames.Web.Games
 
         public override object ToClient() {
             return new {
-                board = _board.Select(b => b.ToClient()),
-                activePlayerName = ActivePlayer?.Name
+                board = Board.Select(ab => ab.ToClient()),
+                activePlayerName = ActivePlayer?.Name,
+                isFinalized = IsFinalized,
+                winningBoxes = WinningBoxes.Select(ab => ab.ToClient()),
+                winnerName = WinnerName
             };
         }
 
-        private class Box
+        private static List<int[]> _winningBoxCombinations = new List<int[]>() {
+            new int[] { 11, 12, 13 }, new int[] { 21, 22, 23 }, new int[] { 31, 32, 33 },
+            new int[] { 11, 21, 31 }, new int[] { 12, 22, 32 }, new int[] { 13, 23, 33 },
+            new int[] { 11, 22, 33 }, new int[] { 13, 22, 31 }
+        };
+
+        public class Box
         {
             public int Row { get; set; }
             public int Col { get; set; }
-            public string PlayerName { get; set; }
+            public int Id { get { return Row * 10 + Col; } }
 
             public Box(int row, int col) {
                 Row = row;
                 Col = col;
             }
+        }
+
+        public class AssignedBox
+        {
+            public Box Box { get; set; }
+            public string PlayerName { get; set; }
+
+            public AssignedBox(int row, int col) {
+                Box = new Box(row, col);
+            }
 
             public object ToClient() {
                 return new {
-                    row = Row,
-                    col = Col,
+                    row = Box.Row,
+                    col = Box.Col,
                     playerName = PlayerName
                 };
             }
-
         }
     }
 }
--- TableGames.Web/TG.Scripts/games/ticTacToe.js
+++ TableGames.Web/TG.Scripts/games/ticTacToe.js
@@ -11,8 +11,6 @@
         self.playerName1 = table.playerNames()[0];
         self.playerName2 = table.playerNames()[1];
 
-        table.activePlayerName(gameState.activePlayerName);
-
         var symbols = {};
         symbols[self.playerName1] = 'X';
         symbols[self.playerName2] = 'O';
@@ -23,27 +21,38 @@
             indices.forEach(function(col) {
                 self.board[row][col] = ko.observable();
                 self.board[row][col].symbol = ko.computed(function() { return symbols[self.board[row][col]()]; });
+                self.board[row][col].isWinning = ko.observable();
             });
         });
         gameState.board.forEach(function(boxState) {
             self.board[boxState.row][boxState.col](boxState.playerName);
         });
 
+        table.activePlayerName(gameState.activePlayerName);
+        self.isFinalized = ko.observable(gameState.isFinalized);
+        gameState.winningBoxes.forEach(function(ab) { self.board[ab.row][ab.col].isWinning(true); });
+        self.winnerName = ko.observable(gameState.winnerName);
+
         self.assign = function(row, col) {
-            if (table.activePlayerName() !== authentication.userName()) {
+            if (table.activePlayerName() !== authentication.userName() || self.isFinalized()) {
                 return;
             }
             var gameChangeParameters = {
                 row: row,
                 col: col
             };
-            gameConfig.sendChangeToServer('AssignBox', gameChangeParameters);
+            gameConfig.sendChangeToServer('assignBox', gameChangeParameters);
         };
 
         self.change = function(playerName, eventName, gameChangeResults) {
-            if (eventName === 'AssignBox') {
+            if (eventName === 'assignBox') {
                 self.board[gameChangeResults.row][gameChangeResults.col](playerName);
                 table.activePlayerName(gameChangeResults.activePlayerName);
+                if (gameChangeResults.isFinalized) {
+                    self.isFinalized(gameChangeResults.isFinalized);
+                    gameChangeResults.winningBoxes.forEach(function(ab) { self.board[ab.row][ab.col].isWinning(true); });
+                    self.winnerName(gameChangeResults.winnerName);
+                }
             }
         };
     }
